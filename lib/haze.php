5f8a7fbe5b55303c59a9d0fc2df2bd9eab3ad46e
Ryan Priebe <ryan@wearefree.ca>
2014-05-06 10:33:00 -0600

    Fixed some bugs that resulted in fatal errors, rewrote the _getRandomWord method to flatten the WHOLE _pool instead of just one random section of it

--- lib/haze.php
+++ lib/haze.php
@@ -5,17 +5,39 @@
 	use \SpaceLord\Generic;
 	use \SpaceLord\GenericList;
 
-	include "generic.php";
-	include "genericlist.php";
+	require "generic.php";
+	require "genericlist.php";
+	require "api.php";
 
-	include "api.php";
-
-	class Haze extends Generic {
+	class Haze {
+		/**
+		 * Stores configuration data
+		 * @var Generic object
+		 */
 		private $_config;
+
+		/**
+		 * Stores the API class data
+		 * @var API object
+		 */
 		private $_api;
+
+		/**
+		 * The pool of words used to generate the password
+		 * @var array
+		 */
 		private $_pool = array();
+
+		/**
+		 * The final password
+		 * @var string
+		 */
 		private $_password;
 
+		/**
+		 * Initialize the object and build the initial pool of words
+		 * @param array $config Configuration options
+		 */
 		public function __construct($config = array()){
 			//setup the configuration object
 			$this->_config = new Generic();
@@ -32,27 +54,43 @@
 			return $this->__toString();
 		}
 
+		/**
+		 * Request a new pool from the API service
+		 * @param  string $word A word to base the entire pool of words off of
+		 * @return array
+		 */
 		private function _request($word = "free"){
 			$str = sprintf("https://words.bighugelabs.com/api/2/%s/%s/%s", $this->_api->getKey(), $word, $this->_config->get("format"));
 
-			if($word != null){
-				$_request = file_get_contents($str);
+			try {
+				if($word != null){
+					if($_request = @file_get_contents($str)){
 
-				if(strlen($_request) > 0){
-					return (array) json_decode($_request);
+						if(strlen($_request) > 0){
+							return (array) json_decode($_request);
+						}
+					}else {
+						throw new \Exception("Rate Limit Exceeded!  No more API requests for a while.");
+					}
 				}
+			}catch(\Exception $e){
+				die($e->getMessage());
 			}
-
-			return false;
 		}
 
+		/**
+		 * Process the pool and pull a random word from it
+		 * @return string
+		 */
 		private function _getRandomWordFromPool(){
 			if(sizeof($this->_pool) > 0){
 				//flatten data array into a searchable format
 				$flattened = array();
 
-				foreach(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($this->_pool)) as $k=>$v){
-					$flattened[$k] = $v;
+				foreach($this->_pool as $d0){
+					foreach($d0 as $key => $value){
+						$flattened = array_merge($flattened, $value);
+					}
 				}
 
 				//ensure there are no duplicates
@@ -61,8 +99,7 @@
 				//choose a random value from the flattened array and process
 				//it to remove spaces (if there are any)
 				$random_word = $flattened[rand(0, (sizeof($flattened) - 1))];
-				//$random_word = str_replace(" ", "", $random_word);
-
+				
 				return $random_word;
 			}
 
@@ -80,12 +117,18 @@
 			return $this->_pool;
 		}
 
+		/**
+		 * Generate the password from the length provided in the configuration
+		 * settings and join the words together with a separator (if required)
+		 * @return string  The final password
+		 */
 		private function _generatePassword(){
 			$list = new GenericList();
 			$list->push($this->_getRandomWordFromPool());
 
 			$i = $list->length;
 			$len = $this->_config->get("length");
+			
 			while($len > $i){
 				//base the next random word on the next item in the list
 				$this->_resetPool($list->indexOf($i));
@@ -101,6 +144,10 @@
 			return $this->_password;
 		}
 
+		/**
+		 * Override method
+		 * @return string  The final password
+		 */
 		public function __toString(){
 			return $this->_password;
 		}
