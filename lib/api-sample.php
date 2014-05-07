4c6308bfe200064607286cea4c4301ca01933547
Ryan Priebe <ryan@wearefree.ca>
2014-05-07 11:04:29 -0600

    Added documentation to api-sample.php

--- lib/api-sample.php
+++ lib/api-sample.php
@@ -2,10 +2,21 @@
 
 	namespace Free;
 
-	use \SpaceLord\Generic;
+	class API {
+		/**
+		 * Your private API key from https://words.bighugelabs.com/
+		 * Private so it is read-only
+		 * @var string
+		 */
+		private $_key = "";
 
-	class API extends Generic {
-		private $_key = "YOUR_API_KEY";
+		/**
+		 * Get the read-only API key
+		 * @return string
+		 */
+		public function getKey(){
+			return $this->_key;
+		}
 	}
 
 ?>
